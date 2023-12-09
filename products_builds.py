ad322e8636c69c3ca3055385eaa41ec99ec9fa54
GitHub Action <action@github.com>
2023-12-09 06:02:44 +0000

    Updated DB as of - 2023-12-09 06:02:44 UTC

--- products_builds.py
+++ products_builds.py
@@ -115,8 +115,8 @@ async def main(start_product_id, end_product_id, operating_system, generation):
         subprocess.run(["git", "push"])
 
 if __name__ == "__main__":
-    start_product_id = 355000000  # Replace with your start product ID
-    end_product_id = 356000000   # Replace with your end product ID
+    start_product_id = 356000000  # Replace with your start product ID
+    end_product_id = 357000000   # Replace with your end product ID
     os_type = "windows"   # Replace with your operating system
     generation = 2        # Replace with your generation
     asyncio.run(main(start_product_id, end_product_id, os_type, generation))
